86b460e76d5462079dbf5e1e009408e61c4b0f9b
Naqii <hassangondal6512@gmail.com>
2026-03-25 03:13:09 -0700

    'image'

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -69,13 +69,13 @@ const stats = [
 /* HERO */
 .hero {
   height: 100vh;
-  background: url("https://scontent.flhe2-4.fna.fbcdn.net/v/t39.30808-6/473222080_1024020919742774_8475328420418101190_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=2a1932&_nc_ohc=hbQLlMjmx5AQ7kNvwGegLZs&_nc_oc=AdlXT9Hzgdu1Cxuw8yRTKt4qJ4kQi8QP5l-N_F_47Br-xRFoaIOkrEKKo4qTV6lba0I&_nc_zt=23&_nc_ht=scontent.flhe2-4.fna&_nc_gid=skK-pjXyo4RTEe7iWG9WKA&_nc_ss=8&oh=00_Afw324RJAM7dnMn5fkyjoJuZeb2kEAmvfcwCUx_vQTkYMw&oe=69C06C62c") no-repeat center/cover;
+  background: url("../assets/punjab-img.jpg") no-repeat center/cover;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-
+/* https://scontent.flhe2-4.fna.fbcdn.net/v/t39.30808-6/473222080_1024020919742774_8475328420418101190_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=2a1932&_nc_ohc=hbQLlMjmx5AQ7kNvwGegLZs&_nc_oc=AdlXT9Hzgdu1Cxuw8yRTKt4qJ4kQi8QP5l-N_F_47Br-xRFoaIOkrEKKo4qTV6lba0I&_nc_zt=23&_nc_ht=scontent.flhe2-4.fna&_nc_gid=skK-pjXyo4RTEe7iWG9WKA&_nc_ss=8&oh=00_Afw324RJAM7dnMn5fkyjoJuZeb2kEAmvfcwCUx_vQTkYMw&oe=69C06C62c */
 .hero::before {
   content: "";
   position: absolute;
